8c16760bf93382e3ef50b7cbbf941dd7de90f220
Humphrey Chiramba <chirambaht@gmail.com>
2021-03-18 21:31:04 +0200

    ahh!

---
+++ src/pages/common/navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <div>
+      <a href="#">Home</a>
+      <a href="#">Products</a>
+      <a href="#">About</a>
+      <a href="#">Cart</a>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h2>Shopping under the stars</h2>
+      <p>This is the site of all sites</p>
+    </div>
+  );
+};
+
+export default Home;
