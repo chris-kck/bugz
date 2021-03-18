623f95c552e678a95723393c041f45ee36559ac8
Shingirai98 <https://github.com/Shingirai98>
2021-03-18 23:52:49 +0200

    this one is for h

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React , {useState, useEffect }from 'react';
 import { Row, Col } from 'react-bootstrap';
 import products from '../../products';
 import Product from '../Product';
+import axios from 'axios';
+
 
 function Home() {
+  
+  /*const [products, setProducts] = useState([])
+
+  useEffect(() =>{
+
+    async function fetchProducts(){
+
+    
+      const {data} = await axios.get('http://blahblah')
+      setProducts(data)
+    }
+
+    fetchProducts()
+  }, [])*/
   return (
     <div>
-      <h1>Latest Products</h1>
+      
       <Row>
         {products.map(product => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
--- src/components/screens/ProductScreen.js
+++ src/components/screens/ProductScreen.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React ,{useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap';
 import Rating from '../../components/Rating';
 import products from '../../products';
+import axios from 'axios';
 
 function ProductScreen( {match} ) {
+  
+  /*const [products, setProducts] = useState([])
+
+  useEffect(() =>{
+
+    async function fetchProducts(){
+
+    
+      const {data} = await axios.get('http://blahblah/${match.params.id}')
+      setProducts(data)
+    }
+
+    fetchProducts()
+  }, [])*/
   const product = products.find((p) => p._id == match.params.id)
   return (
     <div>
