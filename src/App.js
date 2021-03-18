229638f1d2759990f9a947458c949be0b5366108
Shingirai98 <https://github.com/Shingirai98>
2021-03-18 19:15:48 +0200

    Products display ie.images,rating and price

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Container } from 'react-bootstrap'
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './components/screens/Home';
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <Header/>
       <main className="py-3">
         <Container>
-          <h1>Werukamu to Bugz Store</h1>
+          <h1>Werukamu to the Bugz Store</h1>
+          <Home/>
         </Container>
       </main>
       <Footer/>
