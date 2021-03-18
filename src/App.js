f52053cb3d45870163199412868ba32e25cd5af8
Shingirai98 <https://github.com/Shingirai98>
2021-03-18 20:28:02 +0200

    Added routing to links and products

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import { Container } from 'react-bootstrap'
-
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/screens/Home';
+import ProductScreen from './components/screens/ProductScreen';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header/>
       <main className="py-3">
         <Container>
           <h1>Werukamu to the Bugz Store</h1>
-          <Home/>
+          <Route path='/' component={Home} exact/>
+          <Route path='/product/:id' component={ProductScreen} exact/>
         </Container>
       </main>
       <Footer/>
-    </div>
+    </Router>
 
   );
 }
